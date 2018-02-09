1cf14108e096cb578608216c68876595b6add48a
blackrainlearn <2743558475@qq.com>
2018-02-09 17:17:38 +0800

    init

---
+++ Assets/Scripts/Define.cs
@@ -0,0 +1,21 @@
+﻿
+public enum NetState
+{
+    DisConnected = 0,
+    Connecting,
+    Connected,
+    Droped
+}
+
+public struct BufferInfo
+{
+    public int bufferSize;
+    public int readSize;
+    public int specialFlag;
+}
+
+public class NetDefine
+{
+    public static int MAX_RECEIVER_BUFFER_LENGTH = 64 * 1024;   //64k
+    public static int MAX_SEND_BUFFER_LENGTH = 10240;   // 10k
+}
---
+++ Assets/Scripts/TcpConnect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+public class TcpConnect
+{
+    const int TCP_CONNECT_TIMEOUT = 5 * 1000;
+
+    public Action<Stream> ReceiveMsgAction;
+
+    Socket _socket;
+
+    bool _noDelay = false;
+
+    String _serverDomainName = string.Empty;
+    int _serverPort = 0;
+
+    NetState _curNetState = NetState.DisConnected;
+
+    byte[] _tcpHeadBuffer = new byte[4];
+    byte[] _receiveBuffer = new byte[NetDefine.MAX_RECEIVER_BUFFER_LENGTH];
+    BufferInfo _bufferInfo = new BufferInfo();
+    MemoryStream _receiveStream;
+    MemoryStream _uncompressStream;
+
+    byte[] _sendBuffer = new byte[NetDefine.MAX_SEND_BUFFER_LENGTH];
+    MemoryStream _sendStream;
+    byte[] _encryptionKey = new byte[1] { 0 };
+
+    int _sendMsgCount = 0;
+    int _recevierMsgCount = 0;
+
+
+
+}
